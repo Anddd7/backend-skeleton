f7c0fa99a33582d60dd3c13c61f94b80145c4e93
Anddd7 <liaoad_space@sina.com>
2020-01-14 18:09:47 +0800

    clean up project files

--- kotlin-spring-webflux/src/test/kotlin/com/github/anddd7/TestApplication.kt
+++ kotlin-spring-webflux/src/test/kotlin/com/github/anddd7/TestApplication.kt
@@ -1,8 +1,6 @@
 package com.github.anddd7
 
-import com.github.anddd7.configuration.SecurityConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Import
 
 @SpringBootApplication
 internal class TestApplication
